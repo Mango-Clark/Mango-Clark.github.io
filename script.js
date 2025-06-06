3a9cf74b1cfcc2323d97e126516a4c81c79ad37a
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2025-06-07 05:14:42 +0900

    Keep header solid on notes page

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const nav = document.querySelector(".navigationbar");
 
 document.addEventListener("scroll", () => {
-	if (window.scrollY == 0) {
-		nav.classList.remove("scrolled");
-		nav.classList.remove("active");
-		nav.classList.add("top");
-	} else if (window.scrollY < 100) {
-		nav.classList.remove("scrolled");
-		nav.classList.remove("top");
-		nav.classList.add("active");
-	} else {
-		nav.classList.remove("top");
-		nav.classList.remove("active");
-		nav.classList.add("scrolled");
-	}
+        if (nav.classList.contains("static")) {
+                return;
+        }
+        if (window.scrollY == 0) {
+                nav.classList.remove("scrolled");
+                nav.classList.remove("active");
+                nav.classList.add("top");
+        } else if (window.scrollY < 100) {
+                nav.classList.remove("scrolled");
+                nav.classList.remove("top");
+                nav.classList.add("active");
+        } else {
+                nav.classList.remove("top");
+                nav.classList.remove("active");
+                nav.classList.add("scrolled");
+        }
 });
